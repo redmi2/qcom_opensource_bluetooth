59081450d9dc0f3d6dbce71e490aea49446e305a
Igor Risteski <pirej30@yahoo.com>
2018-03-29 01:39:11 +0200

    Don't build it for now
    Change-Id: Ia6781802c930cab51b49b51459a70cf737b8a095

--- tools/Android.mk
+++ tools/Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
+ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),false)
 LOCAL_PATH:= $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif # BOARD_HAVE_BLUETOOTH_QCOM
